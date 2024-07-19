0eb44c1c891fd142bcef6c2330851572adc686f8
paska <lxpaskar@gmail.com>
2024-07-20 00:40:35 +0200

    homepage fix;

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from .views import AnnouncementViewSet, documentation
+from .views import AnnouncementViewSet
 
 
 schema_view = get_schema_view(
@@ -24,5 +24,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include(router.urls)),
-    path('documentation/', documentation, name='documentation'),
 ]
--- richamster/urls.py
+++ richamster/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from api.views import documentation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', documentation, name='documentation'),
     path('api/', include('api.urls')),
 ]
